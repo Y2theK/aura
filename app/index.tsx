2d05fc79f2e3375676319ff02a4f8b927ade19f9
Y2theK <yeyintkyaw.dev@gmail.com>
2024-12-11 21:03:23 +0630

    setup: auth folder

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
 export default function Index() {
   return (
     <View
       className="bg-gray-100 flex-1 items-center justify-center"
     >
+
       <Text className="text-3xl font-semibold text-primary font-pblack">Aura</Text>
-      <StatusBar style="auto"></StatusBar>
+      <StatusBar backgroundColor="#161622" style="light" />
+      <Link href="/home">Home</Link>
     </View>
+    
   );
 }
